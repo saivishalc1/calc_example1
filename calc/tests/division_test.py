8aa7b7f4910fc78fc73847b377de167aa7780df6
sai <sc32@njit.edu>
2021-11-16 12:11:06 -0500

    Update: Added Content to Files

--- calc/tests/division_test.py
+++ calc/tests/division_test.py
@@ -1,6 +1,8 @@
 """Testing Division"""
 import pytest
+
 from calc.calculations.division import Division
+
 
 def test_calculation_subtraction():
     """testing that our calculator has a static method for multiplication"""
@@ -8,7 +10,8 @@ def test_calculation_subtraction():
     division = Division(nums)
     assert division.get_result() == 4.0
 
-def Value_error_test():
+
+def value_test():
     """This shows when not using a number for division"""
     nums = ("1.0", "1.0")
     error1_test = Division(nums)
@@ -16,12 +19,11 @@ def Value_error_test():
         error1_test.get_result()
     assert error1.type == ValueError
 
-def Zero_division_error_test():
+
+def division_test():
     """This shows when divided by 0 there will be an error that happens"""
     nums = (1.0, 0.0)
     error1_test = Division(nums)
     with pytest.raises(ZeroDivisionError) as error2:
         error1_test.get_result()
     assert error2.type == ZeroDivisionError
-
-
